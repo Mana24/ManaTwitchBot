924e8dd145332b53777fcd74ac148622a6346164
Mana <manaabdo333@gmail.com>
2022-11-13 18:19:21 +0200

    SayHi is no longer modertor bound

--- src/commands/simpleCommands.js
+++ src/commands/simpleCommands.js
@@ -1,14 +1,13 @@
-import { getSecondaryCommand, removeAtSymbol, isModOrBroadcaster } from "../utils.js"
+import { getSecondaryCommand, removeAtSymbol } from "../utils.js"
 
 function SayHi({ user, words, msg }) {
    const secondaryCommand = getSecondaryCommand(words);
    let userToGreet = user;
-   // Only mods or the broadcaster should be able to tell the bot who to greet
-   if (secondaryCommand === "to" && isModOrBroadcaster(msg)) { 
+   if (secondaryCommand === "to") { 
       if (!words[2]) return `@${user}, Who should I say hi to?`
       userToGreet = removeAtSymbol(words[2]);
    }
-   return `Hi @${userToGreet}, I am ManaBot :) I do things`
+   return `Hi @${userToGreet}, I am ManaBot :) I do things smileW`
 }
 
 export default [
