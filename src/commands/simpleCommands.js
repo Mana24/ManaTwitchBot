9c962086c45fd849512233c7a7cac0470ffb3b54
Mana <manaabdo333@gmail.com>
2023-05-16 06:05:14 +0300

    Precept progress now persists across process restarts

--- src/commands/simpleCommands.js
+++ src/commands/simpleCommands.js
@@ -1,6 +1,7 @@
 import { getSecondaryCommand, removeAtSymbol, __dirname } from "../utils.js";
 import { currentTrack } from '../spotify.js';
 import { readFile } from "fs/promises"; 
+import storage from 'node-persist'
 import path from "path";
 
 function SayHi({ user, words, msg }) {
@@ -33,5 +34,10 @@ export default [
    // ["leaderboard", ({user}) => `@${user}, Check the lembit leaderboard at streamelements.com/serboggit/leaderboard`],
    ["song", Song],
    ["icecream", () => "Ice cream and Hitless runs have a lot in common! Any% is like sweet cream, simple and quick. Vanilla represents the base game bosses, while chocolate adds DLC complexity. Strawberry is a unique challenge for SL1/BL4 runs, and Neopolitan combines it all. All Achievements is mint chocolate with chips, while Sorbet is for limited categories. Special categories are like frozen yogurt - a unique take on the classic. 🍦"],
-   ["charms", async ({user}) => `@${user}, ${await Precept(preceptIndex++ % 57)}`]
+   ["charms", async ({user}) => {
+      const preceptIndex = await storage.get('preceptIndex') || 0;
+      await storage.update('preceptIndex', (preceptIndex + 1) % 57);
+      return `@${user}, ${await Precept(preceptIndex)}`
+   }],
+   ['resetprecepts', async () => storage.update('preceptIndex', 0)]
 ]
